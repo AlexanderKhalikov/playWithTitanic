95b249b7410b7103c918b5fba94c976755d6c7ab
IBM Aleksandr Khalikov <sasha_khalikov@bk.ru>
2020-09-13 18:45:25 +0300

    sol1

--- Part2.py
+++ Part2.py
@@ -8,6 +8,12 @@ def pprint(smth):
 
 
 # 1) Загрузите выборку из файла titanic.csv с помощью пакета Pandas.
+df = pd.read_csv('titanic/train.csv', index_col='PassengerId')
+
+
+# 2) Оставьте в выборке четыре признака: класс пассажира (Pclass),
+# цену билета (Fare), возраст пассажира (Age) и его пол (Sex).
+
 
 
 
